c55bfa6ef25c7d1dea2db04762512e743b088987
warpousa <warpousa@warpomedia.com>
2024-10-10 06:42:59 -0400

    Latest refactoring and additions...

--- js/surge-script.js
+++ js/surge-script.js
@@ -1,28 +1,40 @@
-/* Start SURGE Javascript Customizations */ 
+	/////* Start SURGE Javascript Customizations */////
+    ///////////////////////////////////////////////////
+    // Misc.Inital DOM manipulation and/or alteration //
+	document.querySelector('.credit').classList.remove();
+	document.getElementById('header').classList.add('is-transparent');
+	//////////////////////////////////////////////////
 
-// Assign random classes to specifc element(s) //
-let classes = new Array ('bground1', 'bground2', 'bground3', 'bground4', 'bground5');
-let length  = classes.length;
-let bgroundImage = jQuery('.banner');
+	// Assign random classes to specifc element(s) //
+	let classes = new Array ('bground1', 'bground2', 'bground3', 'bground4', 'bground5');
+	let length  = classes.length;
+	let bgroundImage = document.querySelectorAll('.banner');
+	bgroundImage.forEach(function(value) {
+		value.classList.add(classes[Math.floor(Math.random() * length)]);
+	});	
+	/////////////////////////////////////////////////
+	
+	// When user scrolls down, hide the navbar. When user scrolls up, show the navbar //
+	document.getElementById('header').classList.add('show-header');
+	let prevScrollpos = window.pageYOffset;
 
-jQuery.each( bgroundImage, function(key, value) {
-	jQuery(value).addClass( classes[ Math.floor ( Math.random() * length ) ] );
-});
+	window.onscroll = function() {		
+		let currentScrollPos = window.pageYOffset;
+		if (prevScrollpos > currentScrollPos) {
+			document.getElementById('header').classList.add('show-header');
+			document.getElementById('header').classList.remove('hide-header');
+			let pageYOffsetValue = window.pageYOffset;
 
-// When user scrolls down, hide the navbar. When user scrolls up, show the navbar //
-
-document.getElementById("header").classList.add("show-header");
-let prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-	let currentScrollPos = window.pageYOffset;
-	if (prevScrollpos > currentScrollPos) {
-		document.getElementById("header").classList.add("show-header");
-		document.getElementById("header").classList.remove("hide-header");
-	} else {
-		document.getElementById("header").classList.add("hide-header");
-		document.getElementById("header").classList.remove("show-header");
+			function applyTransparency() {
+				let applyTransparrency = (pageYOffsetValue < 400) ?  
+					document.getElementById('header').classList.add('is-transparent') : document.getElementById('header').classList.remove('is-transparent');
+			}	
+			applyTransparency();
+		} else {
+			document.getElementById('header').classList.add('hide-header');
+			document.getElementById('header').classList.remove('show-header');
+		}
+		prevScrollpos = currentScrollPos;
 	}
-	prevScrollpos = currentScrollPos;
-}
-
-/* End SURGE Javascript Customizations  */
+	///////////////////////////////////////////////////
+	/////* End SURGE Javascript Customizations  *//////
