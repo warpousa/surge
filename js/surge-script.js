73ecd0e5e87f9e1066757da9afe805d714c0a51d
warpousa <warpousa@warpomedia.com>
2025-10-15 12:30:41 -0400

    Various updates.

--- js/surge-script.js
+++ js/surge-script.js
@@ -42,9 +42,20 @@
 	///////////////////////////////////////////////////
 	// Div cover for background //
 	const menu = document.querySelector('#menu-menu-1');
+	const menuIcon = document.querySelector('#menu-icon.menu-icon');
 	const content = document.querySelector('#content');
 	let overlay = null;
 	//let fadeTimer = null;
+	function createOverlay() {
+		if (!overlay) {
+			overlay = Object.assign(document.createElement('div'), {
+				className: 'bg-coverup',
+				style: 'width:100%; height: 100vh; background: #b8b8b8b2; position: fixed; top: 0; z-index: 0; left: 0; opacity: 0;'
+			});
+			content.appendChild(overlay);
+			fadeIn(overlay);
+		}
+	}
 	function fadeIn(el) {
 		let start = null;
 		function step(ts) {
@@ -73,19 +84,36 @@
 	menu.querySelectorAll('a').forEach(anchor => {
 		['mouseenter', 'click', 'touchstart'].forEach(evt => {
 			anchor.addEventListener(evt, () => {
-				if (!overlay) {
-					overlay = Object.assign(document.createElement('div'), {
-						className: 'bg-coverup',
-						style: 'width:100%; height: 100vh; background: #b8b8b8b2; position: fixed; top: 0; z-index: 0; left: 0; opacity: 0;'
-					});
-					content.appendChild(overlay);
-					fadeIn(overlay);
-				}
+				createOverlay();
 			});
 		});
 	});
 	menu.addEventListener('mouseleave', () => {
 		if (overlay) fadeOutAndRemove(overlay);
 	});
+	menuIcon.addEventListener('click', () => {
+		setTimeout(() => {
+			if (menuIcon.classList.contains('active')) {
+				createOverlay();
+			} else {
+				if (overlay) fadeOutAndRemove(overlay);
+			}
+		}, 10); // small delay to allow class toggle to complete
+	});
+	document.querySelector('#menu-icon').addEventListener('click', () => {
+		document.querySelector('.site-header').classList.toggle('menu-expanded');
+	});
+	document.addEventListener("DOMContentLoaded", () => {
+		const menuIcon = document.getElementById("menu-icon");
+		if (menuIcon && menuIcon.getAttribute("href") === "#") {
+			menuIcon.removeAttribute("href");
+		}
+	});
+	let clickEvent = ('ontouchstart' in window) ? 'touchstart' : 'click';
+	document.addEventListener(clickEvent, function(e) {
+		if (e.target.classList.contains('bg-coverup')) {
+			menuIcon.click();
+		}
+	});   
     ///////////////////////////////////////////////////
 	/////* End SURGE Javascript Customizations  *//////
